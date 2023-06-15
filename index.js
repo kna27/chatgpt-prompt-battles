13ab9a10f03c4404b5d24b4b4c5469a2c2b8f929
kna27 <krisharora27@gmail.com>
2023-06-15 02:47:14 -0400

    change to express + hbs

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+}
+);
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+}
+);

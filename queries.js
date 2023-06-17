91828e5343edd7c9eebe499e1d2a6d796a865384
Krish Arora <krisharora27@gmail.com>
2023-06-17 18:13:15 -0400

    Add creating challs and global display

--- queries.js
+++ queries.js
@@ -10,6 +10,16 @@ async function getUser(id) {
     return result.rows[0];
 }
 
+async function getUserByAuth0Id(auth0Sid) {
+    const query = `SELECT * FROM users WHERE auth0_user_key = $1`;
+    const values = [auth0Sid];
+    const result = await pool.query(query, values);
+    if (result.rowCount === 0) {
+        return null;
+    }
+    return result.rows[0];
+}
+
 async function createUser(auth0Sid, username) {
     const query = `INSERT INTO users (auth0_user_key, username) VALUES ($1, $2)`;
     const values = [auth0Sid, username];
@@ -22,8 +32,30 @@ async function updateUsername(auth0Sid, username) {
     await pool.query(query, values);
 }
 
+async function addChallenge(auth0Sid, name, prompt) {
+    let user = await getUserByAuth0Id(auth0Sid);
+    const query = `INSERT INTO challenges (user_id, name, prompt) VALUES ($1, $2, $3)`;
+    const values = [user['id'], name, prompt];
+    await pool.query(query, values);
+}
+
+async function getAllChallenges() {
+    const query = `SELECT  c.id AS challenge_id, 
+                        c.name AS challenge_name, 
+                        u.username AS author_name, 
+                        COUNT(s.id) AS solves_count
+                    FROM challenges c
+                    JOIN users u ON u.id = c.user_id
+                    LEFT JOIN solves s ON s.challenge_id = c.id
+                    GROUP BY c.id, u.username;`;
+    const result = await pool.query(query);
+    return result.rows;
+}
+
 module.exports = {
     getUser,
     createUser,
-    updateUsername
+    updateUsername,
+    addChallenge,
+    getAllChallenges
 };
--- routes/challs.js
+++ routes/challs.js
@@ -1,11 +1,32 @@
-// global challenge view page
-
 const express = require("express");
+const Queries = require("../queries.js");
 
 const app = new express.Router();
 
-app.get("/challs", (req, res) => {
-    res.render("challs");
+app.get("/create", (req, res) => {
+    res.render("create");
+});
+
+app.post("/create", async (req, res) => {
+    let user = req.oidc.user;
+    if (!user) {
+        res.redirect("/login");
+        return;
+    }
+    let name = req.body.name;
+    let prompt = req.body.prompt;
+    await Queries.addChallenge(user.sub, name, prompt);
+    res.redirect("/challs");
+});
+
+app.get("/challs", async (req, res) => {
+    let user = req.oidc.user;
+    if (!user) {
+        res.redirect("/login");
+        return;
+    }
+    let allChalls = await Queries.getAllChallenges();
+    res.render("challs", { challs: allChalls });
 });
 
 module.exports = app;
